aeee9dd36d208217df70941bcf9546602513ce56
RifatSubaev <34096490+RifatSubaev@users.noreply.github.com>
2018-12-30 14:51:21 +0600

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//Принимаем постовые данные
+$name=$_POST['user_name'];
+$phone=$_POST['telephone'];
+//Тут указываем на какой ящик посылать письмо
+$to = "horoshi-chelovek@bk.ru";
+//Далее идет тема и само сообщение
+// Тема письма
+$subject = "Заявка с сайта";
+// Сообщение письма
+$message = "
+Имя пользователя: ".htmlspecialchars($name)."<br />
+Телефон: <a href='tel:$phone'>".htmlspecialchars($phone)."</a>";
+// Отправляем письмо при помощи функции mail();
+$headers = "From: stastroi.ru <mail@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail ($to, $subject, $message, $headers);
+// Перенаправляем человека на страницу благодарности и завершаем скрипт
+exit();
+?>
